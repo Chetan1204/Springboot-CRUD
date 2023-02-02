01f9388d96223e2f98c951117b3d5d1008329091
Chetan kharat <chetankharat@gmail.com>
2023-02-02 20:13:25 +0530

    checking

--- src/main/java/com/api/book/bootrestbook/BootrestbookApplication.java
+++ src/main/java/com/api/book/bootrestbook/BootrestbookApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootrestbookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootrestbookApplication.class, args);
+		
+SpringApplication.run(BootrestbookApplication.class, args);
 	}
 
 }
